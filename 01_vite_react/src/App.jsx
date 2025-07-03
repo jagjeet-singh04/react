bb9913366921c2cd966b7aa496284b13636776dc
Jagjeet <jagjeetsingh0424@gmail.com>
2025-07-03 22:03:18 +0530

    basics done

--- 01_vite_react/src/App.jsx
+++ 01_vite_react/src/App.jsx
@@ -1,33 +1,14 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
+import Hello from "./hello"
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
+    <> // this is called fragment in this it is like a div
+      <Hello/>
+      <p> hello this is paragraph</p>
+      <h4> this is H4 </h4>
+      <h4> function name should always be starting in capital letter</h4>
     </>
   )
 }
---
+++ 01_vite_react/src/Hello.jsx
@@ -0,0 +1,8 @@
+function Hello()
+{
+    return (
+        <h3> Hello world  </h3>
+    )
+}
+
+export default Hello ; 
